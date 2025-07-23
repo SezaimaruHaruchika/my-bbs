7c619af2f00b9a1c78212f4fe90e2fc32559fed9
SezaimaruHaruchika <sezaimaru303.kdgo.student@gmail.com>
2025-07-24 05:27:59 +0900

    エントリーポイント

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
+import { prisma } from './middleware/prisma'
+import { Bindings } from './type'
+import mainRouter from './routes'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => c.text("Hello, Hono!"));
+
+// グローバルミドルウェア: 全てのリクエストでPrisma Clientを有効化
+app.use('*', prisma)
+
+// メインルーターを適用
+app.route('/', mainRouter)
 
 export default app
